0c6f8506cbe4f4d4a712135c65c891b7abd9f534
Louis.Poirot <louis.poirot@gmail.com>
2025-01-16 18:40:18 +0100

    added more swagger doc & validation

--- src/beers/dto/create-beer.dto.ts
+++ src/beers/dto/create-beer.dto.ts
@@ -1,20 +1,37 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsPositive, IsString, IsUrl } from "class-validator";
 
 export class CreateBeerDto {
 
+    @ApiProperty({
+        description: 'Nom de la bière',
+        example: 'IPA blonde',
+    })
     @IsNotEmpty()
     @IsString()
     beer: string;
 
+    @ApiProperty({
+        description: 'Prix de la bière',
+        example: 4.5,
+    })
     @IsNumber()
     @IsPositive()
     @IsNotEmpty()
     price: number;
 
+    @ApiProperty({
+        description: 'ID de la brasserie associée',
+        example: 2,
+    })
     @IsNotEmpty()
     @IsNumber()
     brewery_id: number;
 
+    @ApiProperty({
+        description: 'URL de l\'image de la bière',
+        example: 'https://example.com/beer.wepb',
+    })
     @IsNotEmpty()
     @IsUrl()
     imageUrl: string;
