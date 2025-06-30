308d421acffe64f9c0eb0e71ac47d49f816c14f2
louis <louis.poirot@gmail.com>
2025-06-30 14:44:45 +0200

    FIX missing module in beersModule

--- src/beers/beers.module.ts
+++ src/beers/beers.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { BeersService } from './beers.service';
 import { BeersController } from './beers.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RedisModule],
   controllers: [BeersController],
   providers: [BeersService],
 })
-export class BeersModule {}
+export class BeersModule { }
